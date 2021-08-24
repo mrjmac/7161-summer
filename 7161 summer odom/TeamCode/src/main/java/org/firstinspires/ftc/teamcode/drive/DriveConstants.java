6706b0bd2432be9ceb30498536eb3d8fe501aa78
Jeff <43101432+jeffffu@users.noreply.github.com>
2021-08-24 16:52:34 -0500

    finished odom poggers

--- 7161 summer odom/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ 7161 summer odom/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -45,7 +45,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.9685; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 14; // in
+    public static double TRACK_WIDTH = 16.75; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -54,8 +54,8 @@ public class DriveConstants {
      * empirically tuned.
      */
     public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kA = 0.0025;
+    public static double kStatic = 0.01;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -66,7 +66,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 54.66855022514893;
     public static double MAX_ACCEL = 54.66855022514893;
-    public static double MAX_ANG_VEL = Math.toRadians(223.7340857142857);
+    public static double MAX_ANG_VEL = Math.toRadians(150); //43.99042757592491
     public static double MAX_ANG_ACCEL = Math.toRadians(223.7340857142857);
 
 
